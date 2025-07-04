686fa7121b7d314f7f94d02ce19973f71ff6b540
Sidharth sangelia <sidharthsangelia@gmail.com>
2025-07-05 01:39:23 +0530

    changed the middleware for webhook api route made it public

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)', 
+  '/sign-up(.*)', 
+  '/',
+  '/api/webhook/register' // Add this line
+])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
