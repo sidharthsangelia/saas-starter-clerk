a6bb65e25250f524a71b627e1574303c066daea4
Sidharth sangelia <sidharthsangelia@gmail.com>
2025-07-04 22:53:28 +0530

    improved a little bit the middleware

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,27 @@
+import { auth } from '@clerk/nextjs/server'
 import React from 'react'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+export default async function Dashboard() {
 
-export default function Dashboard() {
+  const {userId} = await auth()
   return (
-    <div>yo my man! How are you </div>
+    <div>yo my man! How are you<SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn> {userId} </div>
   )
 }
--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <main className='flex justify-center items-center mx-auto'>
+    <main className='flex justify-center mt-12 items-center mx-auto'>
       <SignIn />
     </main>
   )
